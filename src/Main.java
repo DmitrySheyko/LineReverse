9ef5630149cebb0596cfcdeab23fce486b87b860
DmitrySheyko <Student-ne@yandex.ru>
2022-05-22 01:26:14 +0300

    Feat: rename project

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         StringBuilder s = new StringBuilder("Hello world!");
         StringBuilder result = new StringBuilder();
-// VAR 1
+        // VAR 1
         char[] array = new char[s.length()];
         for (int i = s.length() - 1; i >= 0; i--) {
             result.append(s.charAt(i));
